8e2042c2dd54299f5d5a2c7027fc6b263f455966
sukrit-adhikari-1 <sukrit.adhikari@gmail.com>
2017-01-28 15:17:21 -0500

    params in guide

---
+++ setglobal.m
@@ -0,0 +1,23 @@
+function setGlobal = setglobal()
+%set global variable
+disp('called');
+
+e= 1.602e-19;
+
+kB= 1.38e-23;
+
+T= 298;
+
+NA= 6.02e23;
+
+multi= e*NA;
+	
+	%diffusion coefficients
+
+H = 9.30e-09;
+
+OH= 5.30e-09;
+
+Na= 1.30e-09;
+
+
