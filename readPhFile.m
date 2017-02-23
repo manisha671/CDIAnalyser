1f0974a3b4e8dc920a03550f824b3c9e4577cd04
sukrit-adhikari-1 <sukrit.adhikari@gmail.com>
2017-02-22 22:04:54 -0500

    added display

--- readPhFile.m
+++ readPhFile.m
@@ -33,14 +33,12 @@ mainOutputText = '';
 for ii=var
     OutputText = '';
     for iterate = 1:fileSize(1)
+        currentPosition = iterate;
         if iterate > delay && ( string(ii(1)) == 'value')
-            currentPosition = iterate;
             phValueString = data.(ii{1})(currentPosition:currentPosition);
-            %phValueString = data;
             phValue = double(phValueString);
             valueK = 10^(-1 * (phValue + (currentPosition/60)));
             goalValue = J * ((valueK * valueL)+ (valueM/(valueK * N)) ); 
-            %goalValueStr = string({'Skipping : ' iterate '->' data.(ii{1})(iterate:iterate)});
             OutputText = strcat(OutputText, ' S/No= ');
             OutputText = strcat(OutputText, num2str(iterate));
             OutputText = strcat(OutputText, ' PH= ');
@@ -50,7 +48,15 @@ for ii=var
             OutputText = strcat(OutputText, '___');
             disp(   string({'PH : ' phValue ' Output : ' goalValue}));
         elseif iterate <= delay && ( string(ii(1)) == 'value')
-           	%OutputText = strcat(OutputText, 'skip');
+            phValueString = data.(ii{1})(currentPosition:currentPosition);
+            phValue = double(phValueString);
+           	OutputText = strcat(OutputText, ' S/No= ');
+            OutputText = strcat(OutputText, num2str(iterate));
+            OutputText = strcat(OutputText, ' PH= ');
+            OutputText = strcat(OutputText, num2str(phValue));
+            OutputText = strcat(OutputText, ' VALUE= ');
+            OutputText = strcat(OutputText, 'SKIPPED');
+            OutputText = strcat(OutputText, '___');
             disp(string({'Skipping : ' iterate '->' data.(ii{1})(iterate:iterate)})) 
         end
     end
